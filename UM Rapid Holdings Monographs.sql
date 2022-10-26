e9c22419df3bfddfcfa59e9e5dd6cba121e047dc
ameliasutton <ameliasutton@umass.edu>
2022-10-26 12:26:53 -0400

    Updated Query Filters

--- UM Rapid Holdings Monographs.sql
+++ UM Rapid Holdings Monographs.sql
@@ -1,12 +1,14 @@
+-- Active: 1666295278584@@reportingtest.ddns.umass.edu@6991
 select
-	--string_agg(idents.name || ': ' || instances.identifiers__value, ', ') as "Standard Numbers",
-	string_agg(distinct instances.title::text, 'NEXT') as "Title",
-	string_agg(distinct locations.name::text, 'NEXT' ) as "Location",
-	string_agg(distinct locations.code::text, 'NEXT') as "Location Code",
-	string_agg(distinct holdings.call_number::text, 'NEXT') as "Call Number",
+	
+	string_agg(distinct instances.title::text, '') as "Title",
+	string_agg(distinct locations.name::text, '') as "Location",
+	string_agg(distinct locations.code::text, '') as "Location Code",
+	string_agg(distinct holdings.call_number::text, '') as "Call Number",
 	string_agg(distinct oclc.oclc_val::text, ', ') as "OCLC Number(s)",
 	string_agg(distinct issn.issn_val::text, ', ') as "ISSN(s)",
 	string_agg(distinct isbn.isbn_val::text, ', ') as "ISBN(s)"
+	--string_agg(distinct mat_type.name::text, ', ')
 	--string_agg(distinct statements.holdings_statements__statement::text, ', ') as "Holdings Statement",
 	--string_agg(distinct mat_type."name", ' ') as "Material Type"
 from
@@ -17,8 +19,6 @@ join inventory.instance__t__identifiers as instances on
 	holdings.instance_id = instances.id
 join inventory.location__t as locations on
 	holdings.effective_location_id = locations.id
-join inventory.identifier_type__t as idents on
-	idents.id = instances.identifiers__identifier_type_id
 join inventory.item__t as items on
 	items.holdings_record_id = holdings.id
 join inventory.material_type__t as mat_type on
@@ -40,7 +40,7 @@ left join (
 ) as isbn on instances.id = isbn.inst_id
 where
 	true
-	and idents.name in ('OCLC', 'ISSN', 'ISBN')
-	and mat_type.name in ('Book')
+	and (oclc.oclc_val is not Null OR issn.issn_val is not Null OR isbn.isbn_val is not Null)
+	and mat_type.name in ('Book', 'Map', 'CD-ROM', 'Audio CD', 'LP Phonorecord', 'Archival material', 'Government Publication', 'Score', 'Videocassette', 'Thesis/Dissertation', 'DVD/Blu-ray')
 	and locations.code in ('UEA', 'UMGEN', 'UJUV', 'UNEA', 'UMPER', 'UMLLR', 'USPC', 'UMSTOR')
 group by holdings.id
