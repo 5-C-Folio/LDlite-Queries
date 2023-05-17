2ba7a9d2db33117115c567d89252860ef62657a6
ameliasutton <ameliasutton@umass.edu>
2023-05-17 11:34:42 -0400

    Corrected Location filtering match point

--- Reserve Items.sql
+++ Reserve Items.sql
@@ -1,25 +1,27 @@
 with 
 parameters AS (
     SELECT
-      '{Institution Code (AC, HC, MH, SC, UM)}':: VARCHAR AS institution, --Change this value to the earliest date you want to see
-      '{Semester (YYYY Spring/Summer/Fall Winter)}':: VARCHAR AS semester --Change this value to the latest date you want to see
-  )
+      '{Institution Code (AC, HC, MH, SC, UM)}':: VARCHAR AS institution, --Use this line if using the LDlite Reporting Tool
+      '{Semester (YYYY Spring/Summer/Fall Winter)}':: VARCHAR AS semester --Use this line if using the LDlite Reporting Tool
+	  --'UM':: VARCHAR AS institution, --Use this line if NOT using the LDlite Reporting Tool
+      --'2023 Summer':: VARCHAR AS semester --Use this line if NOT using the LDlite Reporting Tool
+   )
 select
     distinct 
-    reserves.item_id as "Item ID",
-    reserves.copied_item__barcode,
-    reserves.copied_item__title "Title",
-    locations."name" as "Temp Location",
-    courses.course_listing_object__term_id as "Term ID",
-    courses.name,
-    terms."name" as "Term",
-    temploantype."name" 
+    reserves.item_id as "Item ID"
+    --reserves.copied_item__barcode,
+    --reserves.copied_item__title "Title",
+    --locations."name" as "Temp Location",
+    --courses.course_listing_object__term_id as "Term ID",
+    --courses.name,
+    --terms."name" as "Term",
+    --temploantype."name" 
 from
     courses.coursereserves_reserves__t reserves
     join inventory.item__t as items on items.id = reserves.item_id 
-    join inventory.location__t locations on items.temporary_location_id  = locations.id
-    join courses.coursereserves_courses__t courses on reserves.course_listing_id = courses.course_listing_id
-    join courses.coursereserves_terms__t terms on courses.course_listing_object__term_id = terms.id
+    join courses.coursereserves_courses__t as courses on reserves.course_listing_id = courses.course_listing_id
+    join inventory.location__t as locations on courses.course_listing_object__location_id  = locations.id
+    join courses.coursereserves_terms__t as terms on courses.course_listing_object__term_id = terms.id
     join inventory.loan_type__t as temploantype on temploantype.id = items.temporary_loan_type_id
 where locations."name" like (select institution from parameters) || '%'
 and terms."name" = (select semester from parameters)
