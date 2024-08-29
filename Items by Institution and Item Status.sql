2d135383eccf6f89bb1ac41b488cc4bdfe704252
ameliasutton <ameliasutton@umass.edu>
2024-08-29 14:55:47 -0400

    New report to retrieve a list of items by institution and item status

---
+++ Items by Institution and Item Status.sql
@@ -0,0 +1,34 @@
+--Items by Institution and Item Status
+with 
+parameters as (
+select
+	'{Institution Code (AC, HC, MH, SC, UM)}':: VARCHAR AS institution, --Use this line if using the LDlite Reporting Tool
+	'{Item Status}':: VARCHAR AS status --Use this line if using the LDlite Reporting Tool
+	--'UM':: VARCHAR as institution, --Use this line if NOT using the LDlite Reporting Tool
+	--'Missing':: VARCHAR as status --Use this line if NOT using the LDlite Reporting Tool
+   )
+select
+	item.id as "Item ID",
+	item.barcode,
+	substring(regexp_replace(coalesce(item.effective_call_number_components__prefix,''),'\n', '') || regexp_replace(coalesce(item.effective_call_number_components__call_number,''),'\n', '') || regexp_replace(coalesce(item.effective_call_number_components__call_number, ''),'\n', ''), 0, length(regexp_replace(coalesce(item.effective_call_number_components__prefix,''),'\n', '') || regexp_replace(coalesce(item.effective_call_number_components__call_number,''),'\n', '') || regexp_replace(coalesce(item.effective_call_number_components__call_number, ''),'\n', ''))/2+1)  as "Effective Call Number",
+	item.status__name,
+	locations.name as "Item Effective Location",
+	servicepoint.name as "Last Service Point Checked In",
+	item.last_check_in__date_time::date,
+	current_date - item.last_check_in__date_time::date as "Days Since Last Check-in"
+from
+	inventory.item__t as item
+join inventory.location__t as locations on
+	locations.id = item.effective_location_id
+left join inventory.holdings_record__t as holdings on
+	holdings.id = item.holdings_record_id 
+left join inventory.service_point__t as servicepoint on
+	servicepoint.id = item.last_check_in__service_point_id 
+where
+	item.status__name = (SELECT status FROM parameters)
+	and locations.name like (
+	select
+		institution
+	from
+		parameters) || '%'
+order by "Item Effective Location", "Effective Call Number"
